2ea30fb2ec9cefb77c23203a9a2e971784fc5712
joaquin telleria <joaquintelleria@gmail.com>
2021-09-16 00:50:13 +0200

    Update AttributeRandomizer.cs

--- AttributeRandomizer.cs
+++ AttributeRandomizer.cs
@@ -581,7 +581,7 @@ public class AttributeRandomizer : MonoBehaviour
                 ReplaceWeaponWithCard();
             }
 
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(0.2f);
         }
 
 
